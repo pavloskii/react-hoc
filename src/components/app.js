6ed4a54abc03b566c9782db655170d7d93ae50e9
pavloskii <pavlo.igor@gmail.com>
2018-07-01 20:49:58 +0200

    finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Higher order components</div>
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
     );
   }
 }
